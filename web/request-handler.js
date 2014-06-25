cb9430814ab7e6793b6ee5b6f56ad2ae8aecba0f
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-25 16:20:11 -0700

    Handle GET requests for archived websites

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,18 +2,26 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var headers = require('./http-helpers').headers;
 var serveAssets = require('./http-helpers').serveAssets;
+var url = require('url');
 // require more modules/folders here!
 
 
 
 exports.handleRequest = function (req, res) {
+  var pathName = url.parse(req.url).pathname;
+  console.log(pathName);
   var statusCode;
   if(req.method === 'GET'){
-    statusCode = 200;
-    res.writeHead(statusCode, headers);
-
-    serveAssets(res, archive.paths.indexPath);
-    // res.end();
+    if(req.url === '/'){
+      statusCode = 200;
+      res.writeHead(statusCode, headers);
+      serveAssets(res, archive.paths.indexPath);
+    }else if(pathName === '/www.google.com'){
+      statusCode = 200;
+      res.writeHead(statusCode, headers);
+      serveAssets(res, archive.paths.archivedSites + pathName);
+      console.log('i got in');
+    }
   }
   // res.end(archive.paths.list);
 };
