2d2f56db0f3f4a6ac36cb2f0498ceea343497482
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-25 16:07:41 -0700

    Handle GET requests for index.html

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,7 +10,12 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
+
+
 exports.serveAssets = function(res, asset) {
+  console.log(asset);
+  res.end(fs.readFileSync(asset));
+
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,19 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var headers = require('./http-helpers').headers;
+var serveAssets = require('./http-helpers').serveAssets;
 // require more modules/folders here!
 
+
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var statusCode;
+  if(req.method === 'GET'){
+    statusCode = 200;
+    res.writeHead(statusCode, headers);
+
+    serveAssets(res, archive.paths.indexPath);
+    // res.end();
+  }
+  // res.end(archive.paths.list);
 };
