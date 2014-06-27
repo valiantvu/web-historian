afeb9603a67cf88c6cbebb9ec02113b0db406ec8
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-26 17:36:11 -0700

    Implement htmlfetcher.js

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,5 +1,14 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
 var archive = require('../helpers/archive-helpers');
-// res.end();
-// done();
+var fs = require('fs');
+
+var urls = fs.readFileSync(archive.paths.list).toString().split('\n');
+var urlsToGet = [];
+
+for (var i = 0; i < urls.length; i++) {
+  if(!archive.isURLArchived(urls[i])){
+    urlsToGet.push(urls[i]);
+  }
+}
+archive.downloadUrls(urlsToGet);
